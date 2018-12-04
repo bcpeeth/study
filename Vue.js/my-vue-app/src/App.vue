2293489a7c76e4a53241e07684a86ff3c13fad70
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-04 15:46:48 +0100

    web app first live

--- Vue.js/my-vue-app/src/App.vue
+++ Vue.js/my-vue-app/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>{{otherMsg}}</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -24,7 +24,8 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Great way to start your Vue.js learning',
+      otherMsg: 'This was the second header'
     }
   }
 }
