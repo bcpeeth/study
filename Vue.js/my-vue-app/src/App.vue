38687e30fad114860e8defe256bc3655de372fbe
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-04 17:08:37 +0100

    some changes

--- Vue.js/my-vue-app/src/App.vue
+++ Vue.js/my-vue-app/src/App.vue
@@ -3,12 +3,16 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>{{otherMsg}}</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
+    <div>
+      <ul>
+        <li v-for="user in users">
+            <a href="#">
+              {{user.firstname}} {{user.lastname}}
+            </a>
+          </li>
+      </ul>
+    </div>
+    
     <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
@@ -25,7 +29,13 @@ export default {
   data () {
     return {
       msg: 'Great way to start your Vue.js learning',
-      otherMsg: 'This was the second header'
+      otherMsg: 'This was the second header',
+      users: [
+        {firstname: 'Sebastian', lastname: 'Eschweiler'},
+        {firstname: 'Bill', lastname: 'Smith'},
+        {firstname: 'John', lastname: 'Porter'}
+      ],
+      input_val: '',
     }
   }
 }
