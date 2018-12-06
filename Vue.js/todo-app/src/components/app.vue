7bc577c95c0809f7db5ee84ee7af3c5a4e8fab89
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-06 15:33:01 +0100

    Todo List Vue app

---
+++ Vue.js/todo-app/src/components/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>My Todo App!</h1>
+</template>
+
+<script>
+import TodoList from './TodoList.vue'
+export default {
+    components: {
+        TodoList
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../main.scss';
+
+</style>
