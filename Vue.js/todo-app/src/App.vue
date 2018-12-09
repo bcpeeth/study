9813a15dbe46e125b8f270598d359c48b2805f7a
bcpeeth <34841898+bcpeeth@users.noreply.github.com>
2018-12-09 20:19:55 +0100

    imageClick @click action finsihed

--- Vue.js/todo-app/src/App.vue
+++ Vue.js/todo-app/src/App.vue
@@ -1,19 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
 </script>
 
 <style>
